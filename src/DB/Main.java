383f60383ea5cdbb09ac6950c50c35993f9e06c3
Habatoo <habatoo@gmail.com>
2020-08-30 00:43:39 +0700

    14 commit

---
+++ src/DB/Main.java
@@ -0,0 +1,53 @@
+package DB;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//public class Main {
+//    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+//        String url = "jdbc:postgresql://localhost:5432/first_lesson";
+//        String userName = "postgres";
+//        String password = "1234567890";
+//        Class.forName("org.postgresql.Driver");
+
+public class Main {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+        // Задаем параметры подключения
+        String url = "jdbc:postgresql://localhost:5432/first_lesson";
+        String userName = "postgres";
+        String password = "1234567890";
+        Class.forName("org.postgresql.Driver");
+
+        try (Connection con = DriverManager.getConnection(url, userName, password);
+             PreparedStatement pst = con.prepareStatement("SELECT * FROM Book");
+             ResultSet rs = pst.executeQuery()) {
+
+            while (rs.next()) {
+
+                System.out.print(rs.getInt(1));
+                System.out.print(": ");
+                System.out.print(rs.getString(2));
+                System.out.print(", ");
+                System.out.println(rs.getFloat(3) + "\t$");
+            }
+
+        } catch (SQLException ex) {
+
+            Logger lgr = Logger.getLogger(Main.class.getName());
+            lgr.log(Level.SEVERE, ex.getMessage(), ex);
+        }
+    }
+}
+
---
+++ src/DB/books.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS Book
+
+CREATE TABLE IF NOT EXISTS Book (bookId serial PRIMARY KEY, name CHAR(30) NOT NULL, price REAL);
+INSERT INTO Book (name, price) VALUES ('Inferno', 45.0);
+INSERT INTO Book (name, price) VALUES ('Harry Potter', 45.5);
+INSERT INTO Book (name, price) VALUES ('It', 25.0);
+INSERT INTO Book (name, price) VALUES ('Spartacus', 55.0);
+INSERT INTO Book (name, price) VALUES ('Green mile', 20.6);
+INSERT INTO Book (name, price) VALUES ('Solomon day', 5.0);
